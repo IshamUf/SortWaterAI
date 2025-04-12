41abd13e7ed063bbc96f7be5e355817eb0ab8bcc
IshamUf <amishmuratov@gmail.com>
2025-04-12 22:42:41 +0300

    Переименовали колонку в талблице Users

--- react_app/backend/models/User.mjs
+++ react_app/backend/models/User.mjs
@@ -4,9 +4,12 @@ import sequelize from '../config/database.mjs';
 
 const User = sequelize.define('User', {
   username: { type: DataTypes.STRING, unique: true },
-  password: DataTypes.STRING,
+  // Ранее использовалось поле password, теперь переименовано в telegram_id
+  telegram_id: DataTypes.STRING,
   coins: { type: DataTypes.INTEGER, defaultValue: 0 },
   last_daily_reward: DataTypes.DATE,
+}, {
+  tableName: 'Users'
 });
 
 export default User;
